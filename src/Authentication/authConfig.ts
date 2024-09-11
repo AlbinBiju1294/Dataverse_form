585b8eedee9caac1892d7116ab65ec1c924a3113
AlbinBiju1294 <albinbiju1294@gmail.com>
2024-09-11 14:28:16 +0530

    change auth redirect uri

--- src/Authentication/authConfig.ts
+++ src/Authentication/authConfig.ts
@@ -17,7 +17,7 @@ export const msalConfig = {
     clientId: import.meta.env.VITE_CLIENT_ID,
     authority: `https://login.microsoftonline.com/${import.meta.env.VITE_TENANT_ID}`,
     redirectUri: import.meta.env.MODE === 'production'
-      ? "https://lemon-river-096580000.5.azurestaticapps.net"
+      ? "https://mango-beach-05185b400.5.azurestaticapps.net"
       : "http://localhost:5173",
   },
   cache: {
